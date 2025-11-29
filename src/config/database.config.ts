e7a5f3ec459b659086f7df6cd5b67ab5e2436d31
HappyRehund <rayhanardian045@gmail.com>
2025-11-29 22:50:02 +0700

    added migration init database and create table article

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -8,7 +8,8 @@ export const typeOrmConfig = registerAs('database',
     port: parseInt(process.env.DB_PORT ?? '5432'),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    autoLoadEntities: true
   })
 
 )
